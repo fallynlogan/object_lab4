fc689fc0c3b97ceca39ca02d8ba42f114a78e6b1
Fallyn Logan <49659834+fallynlogan@users.noreply.github.com>
2019-10-22 20:53:54 -0600

    Update sketch_oct22a.ino

--- sketch_oct22a/sketch_oct22a.ino
+++ sketch_oct22a/sketch_oct22a.ino
@@ -14,11 +14,11 @@ void loop() {
   // put your main code here, to run repeatedly:
    if (Serial.available() > 0) {   // see if there's incoming serial data
     incomingByte = Serial.read(); // read it
-    if (incomingByte == 'H') {    // if it's a capital H (ASCII 72),
+    if (incomingByte == 'a') {    // if it's a capital H (ASCII 72),
       digitalWrite(ledPin1, HIGH); // turn on the LED
       digitalWrite(ledPin2, LOW);
     }
-    if (incomingByte == 'L') {    // if it's an L (ASCII 76)
+    if (incomingByte == 'l') {    // if it's an L (ASCII 76)
       digitalWrite(ledPin1, LOW);  // turn off the LED
       digitalWrite(ledPin2, HIGH);
     }
